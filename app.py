cddb378448732326af7a87fe46ae68f2950f9a70
laxmisihag3626-beep <laxmisihag3626@gmail.com>
2026-09-15 14:53:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ for index, col_name in enumerate(X.columns):
   all_ans.append(value)
 
 ud = {j:all_ans[i] for i,j in enumerate(X.columns)}
-user_df = pd.DataFrame(all_ans, columns = X.columns)
+user_df = pd.DataFrame([all_ans], columns = X.columns)
 st.write(user_df)
 
 #===================================Prediction===================================
